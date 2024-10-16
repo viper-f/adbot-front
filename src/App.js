ab1199f5998888da6488105832ea6056d0a4adfa
Kate Makarova <kate@donkeymob.com>
2024-10-16 13:52:38 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {Link, Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Header from "./Components/Header";
 
 function App() {
